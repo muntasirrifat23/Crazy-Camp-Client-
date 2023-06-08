cea6ce830fad91937f1b1fd278cdd184f763ea96
Munshi Muntasir Rifat <rifat2002m@gmail.com>
2023-06-08 21:32:07 +0600

    toggle

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
-
+import './Home.css'
 const Home = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleMode = () => {
+        setIsDarkMode(!isDarkMode);
+    };
     return (
         <div>
-           
+           <div className={isDarkMode ? 'dark-mode' : 'light-mode'}>
+      <button onClick={toggleMode} className='mt-20'>Toggle Mode</button>
+      <div className="content">
+        {/* Place your website content here */}
+      </div>
+    </div>
         </div>
     );
 };
